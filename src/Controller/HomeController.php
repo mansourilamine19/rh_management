b5426365b3f3293cc45e1c9f68d6814410628be7
TAS Noureddine <noureddine@data-pulse.fr>
2025-07-02 15:53:19 +0200

    Ajout du frontend

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,6 +6,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Repository\MissionRepository;
+use App\Repository\ContractRepository;
+use App\Repository\UserRepository;
+use App\Repository\RegionRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\SecurityBundle\Security;
 
@@ -19,9 +23,23 @@ class HomeController extends AbstractController {
     }
 
     #[Route('/home', name: 'app_home')]
-    public function index(): Response {
+    public function index(
+        UserRepository $userRepository,
+        ContractRepository $contractRepository,
+        MissionRepository $missionRepository,
+        RegionRepository $regionRepository
+    ): Response {
+        $nbEmployes = $userRepository->count([]);
+        $nbContrats = $contractRepository->count([]);
+        $nbMissions = $missionRepository->count([]);
+        $nbRegions  = $regionRepository->count([]);
+
         return $this->render('home/index.html.twig', [
-                    'controller_name' => 'HomeController',
+            'controller_name' => 'HomeController',
+            'nb_employes' => $nbEmployes,
+            'nb_contrats' => $nbContrats,
+            'nb_missions' => $nbMissions,
+            'nb_regions' => $nbRegions,
         ]);
     }
     
