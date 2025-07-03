76ce94114befabb7ca85335631a66191d478ef06
Noureddine Tas <noureddine.tas@gmail.com>
2025-07-03 17:09:20 +0200

    Dashboard

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,22 +4,21 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Repository\MissionRepository;
 use App\Repository\ContractRepository;
 use App\Repository\UserRepository;
 use App\Repository\RegionRepository;
+use App\Repository\EvaluationRepository; // ✅ ajout du repository
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\SecurityBundle\Security;
 
 class HomeController extends AbstractController {
 
     public function __construct(
-            private readonly Security $security,
-            private readonly EntityManagerInterface $em,
+        private readonly Security $security,
+        private readonly EntityManagerInterface $em,
     ) {
-        
     }
 
     #[Route('/home', name: 'app_home')]
@@ -27,20 +26,23 @@ class HomeController extends AbstractController {
         UserRepository $userRepository,
         ContractRepository $contractRepository,
         MissionRepository $missionRepository,
-        RegionRepository $regionRepository
+        RegionRepository $regionRepository,
+        EvaluationRepository $evaluationRepository 
     ): Response {
         $nbEmployes = $userRepository->count([]);
         $nbContrats = $contractRepository->count([]);
         $nbMissions = $missionRepository->count([]);
         $nbRegions  = $regionRepository->count([]);
+        $evaluations = $evaluationRepository->findAll(); 
 
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'title' => "Dashboard RH",
             'nb_employes' => $nbEmployes,
             'nb_contrats' => $nbContrats,
             'nb_missions' => $nbMissions,
             'nb_regions' => $nbRegions,
+            'evaluations' => $evaluations, 
         ]);
     }
-    
 }
